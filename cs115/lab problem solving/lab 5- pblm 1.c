3f89b3d076fa6bf41570b3a234dd0820a13ebc68
marufur-rashid <68655216+marufur-rashid@users.noreply.github.com>
2020-08-06 12:52:49 +0600

    new problem solve

---
+++ cs115/lab problem solving/lab 5- pblm 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    int i;
+    printf("even number between 1 to 100\n");
+ for (i=2;i<=100;i++){
+    if (i%2==0)
+ printf(" all the even number %d\n",i);}
+ return 0;
+}
