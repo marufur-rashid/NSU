d6b4e8eb5578e5dd86abaf55d35bc3fd4902c4b2
marufur-rashid <68655216+marufur-rashid@users.noreply.github.com>
2020-08-20 19:44:13 +0600

    Update lab 8a- pblm 1.c
    new added

--- cs115/lab problem solving/lab-8a/lab 8a- pblm 1.c
+++ cs115/lab problem solving/lab-8a/lab 8a- pblm 1.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+int main(){
+    int i, x, n, ara[10101];
+    printf("Enter array size\n");
+    scanf("%d", &n);
+
+    printf("array element \n");
+    for(i = 0; i < n; i++) {
+    scanf("%d", &ara[i]);
+    }
+
+    printf("search key\n");
+    scanf("%d", &x);
+
+    int mat = 0;
+    for(i = 0; i < n; i++)
+    {
+        if (x == ara[i])
+        {
+            mat = mat + 1;
+        }
+    }
+
+    printf("search key appears %d times", mat);
+
+    return 0;
+}
+
 
