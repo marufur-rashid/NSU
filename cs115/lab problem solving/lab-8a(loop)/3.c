68cb28d3347bff7c6c196fa0319adfe223502308
marufur-rashid <68655216+marufur-rashid@users.noreply.github.com>
2020-08-25 19:55:17 +0600

    Update 3.c
    new one

--- cs115/lab problem solving/lab-8a(loop)/3.c
+++ cs115/lab problem solving/lab-8a(loop)/3.c
@@ -1,1 +1,47 @@
+#include<stdio.h>
+
+void show_odd(int n)
+{
+    int i;
+    for (i=1; i<=n; i++)
+    {
+        if(i%2==1)
+        {
+            printf("%d ",i);
+        }
+
+    }
+}
+
+
+void show_even(int n)
+{
+    int i;
+    for (i=1; i<=n; i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d ",i);
+
+        }
+    }
+}
+
+
+int main()
+{
+
+    int i, j, n=6;
+    for (i=1;i<=n;i++)
+    {
+        if(i%2 == 0) {
+            show_even(i);
+        }
+        else {
+            show_odd(i);
+        }
+
+        printf("\n");
+    }
+}
 
